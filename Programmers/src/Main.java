a5659dccc2a8486b2f54c926e8eddb45088c8213
Dotton <dotton95@gmail.com>
2022-01-02 21:22:50 +0900

    1/2

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -2,11 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		모의고사 test = new 모의고사();
-		int[] answers = {1,2,3,4,5};
-		int[] result = test.solution(answers);
-		for(int i=0;i<result.length;i++) {
-			System.out.println(result[i]);	
-		}
+		더맵게 test = new 더맵게();
+		int[] scoville = {1,2,3,9,10,12};
+		int k = 7;
+		int result = test.solution(scoville,k);
+		System.out.println(result);	
 	}
 }
---
+++ Programmers/src/더맵게.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class ´õ¸Ê°Ô {
+    public int solution(int[] scoville, int K) {
+ int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue();
+
+        for (int aScoville : scoville) {
+            heap.offer(aScoville);
+        }
+
+        while (heap.peek() <= K) {
+            if (heap.size() == 1) {
+                return -1;
+            }
+            int a = heap.poll();
+            int b = heap.poll();
+
+
+            int result = a + (b * 2);
+
+            heap.offer(result);
+            answer ++;
+        }
+        return answer;
+    }
+    /*
+     	´õ¸Ê°Ô test = new ´õ¸Ê°Ô();
+		int[] scoville = {1,2,3,9,10,12};
+		int k = 7;
+		int result = test.solution(scoville,k);
+		System.out.println(result);	
+     */
+}
